e345f732d0c432870245e9dc15865501730f079a
Jonas Sunde Valfridsson <jonval@xmas.lan>
2018-02-23 20:02:49 +0100

    added sobel-filter and function for combining magnitude of filters

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import numpy as np
+import math
 
 
 def load_image(file_name):
@@ -31,6 +32,7 @@ def rgb2grey(rgb_img):
 
     return grey
 
+
 def convolve(data, convf):
     fsize = int((convf.shape[0]) / 2)
 
@@ -44,13 +46,48 @@ def convolve(data, convf):
     return out
 
 
+def convolve_magnitude(data, convf, shape_x, shape_y):
+    """Applies filters and collects their euclidian magnitude."""
+
+    out = np.zeros((data.shape[0] - shape_x, data.shape[1] - shape_y))
+
+    for row in range(shape_x, out.shape[0]):
+        for col in range(shape_y, out.shape[1]):
+            """Beware of the super line, maybe this should be split up abit xD."""
+            out[row, col] = int(math.sqrt(sum(map(lambda filtr: math.pow(np.sum(data[row:row + shape_x, col: col + shape_y] * filtr), 2), convf))))
+                                   
+    return out
+
+
 img = load_image("res/lenna.png")
 
 grey = rgb2grey(img)
 
+vertical_edge_filter = np.array(
+    [[1.0, 1.0, 1.0, 1.0, 1.0],
+     [2.0, 2.0, 2.0, 2.0, 2.0],
+     [0.0, 0.0, 0.0, 0.0, 0.0],
+     [-2.0, -2.0, -2.0, -2.0, -2.0],
+     [-1.0, -1.0, -1.0, -1.0, -1.0]])
+
+horizontal_edge_filter = np.array(
+    [[1.0, 2.0, 0.0, -2.0, -1.0],
+     [1.0, 2.0, 0.0, -2.0, -1.0],
+     [1.0, 2.0, 0.0, -2.0, -1.0],
+     [1.0, 2.0, 0.0, -2.0, -1.0],
+     [1.0, 2.0, 0.0, -2.0, -1.0]])
+
+"""
+Can maninipulate the convolve_magnitude function for this perticular case to get the
+actual angle of edges but taking TAN och the magnitude of the vertical_filter and horizontal_filter!
+I might add that later.
+"""
+
+sobel_filter = convolve_magnitude(grey, [vertical_edge_filter, horizontal_edge_filter], 5, 5)
+
+
 simple_filter = np.array([[0,0,.1,0,0],[0, .1, .3, .1, 0], [.1, .3, .5, .3 ,.1], [0, .1, .3, .1, 0], [0,0,.1,0,0]])
 simple_filter = simple_filter/np.sum(simple_filter)
-print(simple_filter)
 
 blur_filter = np.array(
     [[0.00000067 , 0.00002292 , 0.00019117 , 0.00038771 , 0.00019117 , 0.00002292 , 0.00000067],
@@ -66,7 +103,6 @@ edge_filter = np.array([[-1, -1, -1], [-1,  8, -1], [-1, -1, -1]])
 blured = convolve(grey, blur_filter)
 edges = convolve(grey, edge_filter)
 
+save_image(sobel_filter, "lenna-sobel.png")
 save_image(grey, "lenna.png")
-save_image(blured, "lenna-blured.png")
-save_image(edges, "lenna-edges.png")
 
